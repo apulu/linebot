84c6733f231ac7d6ff7956b0ce48cb73d3c48e5f
apulu <eric6420998@gmail.com>
2023-03-12 00:44:40 +0800

    Update bot.config.js

--- bot.config.js
+++ bot.config.js
@@ -1,9 +1,9 @@
 const linebot = require('linebot');
 
 var bot = linebot({
-    channelId: '請輸入',
-    channelSecret: '請輸入',
-    channelAccessToken: '請輸入',
+    channelId: '1660729601',
+    channelSecret: '50a41f676728d857765ac69ca22940bd',
+    channelAccessToken: 'hJW0O8WBeZcrTq9Rl23Dks5b25APA2vKCiqrGHksEBKlvrxmUULN2uaLM1eW1Wutk+AWum0sIweEWOUm3jQF5497EMlLVgZnDrGCDqM0hJ4d9Lti1CEyhf4Idm3MKHK1n1Npuz6JteynCcZEjR1mmAdB04t89/1O/w1cDnyilFU=',
 })
 
 module.exports = bot
